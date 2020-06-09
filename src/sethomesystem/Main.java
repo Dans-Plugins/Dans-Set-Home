bfc695afb50cf643cf611ac13b07e41dbab56c1c
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-09 05:05:24 -0600

    initial build

---
+++ src/sethomesystem/Main.java
@@ -0,0 +1,25 @@
+package sethomesystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Medieval Set Home plugin enabling...");
+
+
+
+        System.out.println("Medieval Set Home plugin enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Medieval Set Home plugin disabling...");
+
+
+
+        System.out.println("Medieval Set Home plugin disabled.");
+    }
+
+}
