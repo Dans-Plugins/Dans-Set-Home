82f19116b9f68b56f0382b4fadaebc2977d1d5a3
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-09 06:48:53 -0600

    implemented saving/loading

--- src/sethomesystem/Main.java
+++ src/sethomesystem/Main.java
@@ -2,7 +2,12 @@ package sethomesystem;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main extends JavaPlugin {
 
@@ -28,15 +33,68 @@ public class Main extends JavaPlugin {
     }
 
     public void saveHomeRecordFileNames() {
+        try {
+            File saveFolder = new File("./plugins/Medieval-Set-Home/");
+            if (!saveFolder.exists()) {
+                saveFolder.mkdir();
+            }
+            File saveFile = new File("./plugins/Medieval-Set-Home/" + "home-record-filenames.txt");
+            if (saveFile.createNewFile()) {
+                System.out.println("Save file for home record filenames created.");
+            } else {
+                System.out.println("Save file for home record filenames already exists. Overwriting.");
+            }
 
+            FileWriter saveWriter = new FileWriter(saveFile);
+
+            // actual saving takes place here
+            for (HomeRecord record : homeRecords) {
+                saveWriter.write(record.getPlayerName() + ".txt" + "\n");
+            }
+
+            saveWriter.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving home record filenames.");
+        }
     }
 
     public void saveHomeRecords() {
-
+        for (HomeRecord record : homeRecords) {
+            record.save();
+        }
     }
 
     public void loadHomeRecords() {
+        try {
+            System.out.println("Attempting to load home records...");
+            File loadFile = new File("./plugins/Medieval-Set-Home/" + "home-record-filenames.txt");
+            Scanner loadReader = new Scanner(loadFile);
 
+            // actual loading
+            while (loadReader.hasNextLine()) {
+                String nextName = loadReader.nextLine();
+                HomeRecord temp = new HomeRecord();
+                temp.setPlayerName(nextName);
+                temp.load(nextName + ".txt"); // provides owner field among other things
+
+                // existence check
+                boolean exists = false;
+                for (int i = 0; i < homeRecords.size(); i++) {
+                    if (homeRecords.get(i).getPlayerName().equalsIgnoreCase(temp.getPlayerName())) {
+                        homeRecords.remove(i);
+                    }
+                }
+
+                homeRecords.add(temp);
+
+            }
+
+            loadReader.close();
+            System.out.println("Home records successfully loaded.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error loading the factions!");
+        }
     }
 
 }
