3a98144262ca6de1ac8d7f482cfe43a7bdafe338
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-16 15:57:00 -0600

    made teleporting take 3 seconds and get cancelled upon movement

--- src/main/java/sethomesystem/Main.java
+++ src/main/java/sethomesystem/Main.java
@@ -1,6 +1,7 @@
 package sethomesystem;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,6 +16,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import static org.bukkit.Bukkit.getServer;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -129,8 +132,28 @@ public class Main extends JavaPlugin implements Listener {
                 for (HomeRecord record : homeRecords) {
                     if (record.getPlayerName().equalsIgnoreCase(player.getName())) {
                         if (record.getHomeLocation() != null) {
-                            player.teleport(record.getHomeLocation());
-                            player.sendMessage(ChatColor.GREEN + "Teleporting.");
+                            player.sendMessage(ChatColor.GREEN + "Teleporting in 3 seconds...");
+                            int seconds = 3;
+
+                            Location initialLocation = player.getLocation();
+
+                            getServer().getScheduler().runTaskLater(this, new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (initialLocation.getX() == player.getLocation().getX() &&
+                                            initialLocation.getY() == player.getLocation().getY() &&
+                                            initialLocation.getZ() == player.getLocation().getZ()) {
+
+                                        // teleport the player
+                                        player.teleport(record.getHomeLocation());
+
+                                    }
+                                    else {
+                                        player.sendMessage(ChatColor.RED + "Movement Detected. Teleport cancelled.");
+                                    }
+
+                                }
+                            }, seconds * 20);
                         }
                         else {
                             player.sendMessage(ChatColor.RED + "Home not set!");
