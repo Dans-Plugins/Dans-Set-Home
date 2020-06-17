cefd8708f6abdaeb3bcccc0ecaa9eef5b5cfd9d9
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-16 22:37:25 -0600

    added /msh help, /msh forcesave and /msh forceload

--- src/main/java/sethomesystem/Main.java
+++ src/main/java/sethomesystem/Main.java
@@ -165,6 +165,47 @@ public class Main extends JavaPlugin implements Listener {
             }
         }
 
+        if (label.equalsIgnoreCase("msh")) {
+            // args check
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.RED + "Try /msh help");
+                return false;
+            }
+
+            // help command
+            if (args[0].equalsIgnoreCase("help")) {
+                sender.sendMessage(ChatColor.AQUA + "/help - View a list of helpful commands.");
+                sender.sendMessage(ChatColor.AQUA + "/sethome - Set your home location.");
+                sender.sendMessage(ChatColor.AQUA + "/home - Teleport to your home location.");
+                sender.sendMessage(ChatColor.AQUA + "/forcesave - Force a save from the console.");
+                sender.sendMessage(ChatColor.AQUA + "/forceload - Force a load from the console.");
+            }
+
+            // forcesave
+            if (args[0].equalsIgnoreCase("forcesave")) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage(ChatColor.RED + "This command can only be used from the console.");
+                    return false;
+                }
+
+                System.out.println("Medieval Set Home is saving...");
+                saveHomeRecords();
+            }
+
+            // forceload
+            if (args[0].equalsIgnoreCase("forceload")) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage(ChatColor.RED + "This command can only be used from the console.");
+                    return false;
+                }
+
+                System.out.println("Medieval Set Home is loading...");
+                saveHomeRecordFileNames();
+                saveHomeRecords();
+            }
+
+        }
+
         return false;
     }
 
